ea0610cf7875f0d296b505fa0f6ed1b2642cdde1
Hans <hans@example.com>
2026-04-26 14:13:25 +0700

    Add device_tracker project

---
+++ device_tracker/ios/Runner/AppDelegate.swift
@@ -0,0 +1,19 @@
+import Flutter
+import UIKit
+import GoogleMaps
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // TODO: Ganti 'YOUR_IOS_API_KEY_HERE' dengan API Key asli
+    GMSServices.provideAPIKey("YOUR_IOS_API_KEY_HERE")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+}
